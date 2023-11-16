6a7d58d5e502f37fd06216f098eb43e4ea5631db
BRIGHT <wissen30@hotmail.com>
2023-11-16 21:57:15 +0300

    Add project files.

---
+++ PseudoSelector/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PseudoSelector.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
